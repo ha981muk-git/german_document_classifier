ae1aaf74a54eb3dcc09c70685824899c8118e6ac
Harsh Mukhiya <ha981muk@gmail.com>
2025-11-16 22:34:24 +0100

    bit changes to data loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,9 @@
 # data_loader.py
+import os
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from transformers import AutoTokenizer
 
-import os
 
 script_path = os.path.realpath(__file__)
 script_dir = os.path.dirname(script_path)

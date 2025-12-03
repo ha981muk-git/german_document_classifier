ac7eabcfeebb0b32b53af2067ca691046f4c776d
Harsh Mukhiya <ha981muk@gmail.com>
2025-12-04 00:18:44 +0100

    path updated

--- app/core/paths.py
+++ app/core/paths.py
@@ -8,3 +8,24 @@ APP_DIR = PROJECT_ROOT / "app"
 
 # Add project root for imports
 sys.path.append(str(PROJECT_ROOT))
+
+
+DATA_DIR = APP_DIR / "data"
+
+RAW_DIR = DATA_DIR / "raw"
+SYNTHETIC_DIR = DATA_DIR / "synthetic"
+
+# Output directory (Where the CSV will go)
+PROCESSED_DIR = DATA_DIR / "processed"
+
+# OUTPUT_FILENAME = "labeled_documents.csv"
+
+# Mapping Directory Names -> Target Labels
+# Key = Folder Name, Value = Label for CSV
+LABEL_MAP = {
+    "complaints": "complaint",
+    "contracts": "contract",
+    "invoices": "invoice",
+    "orders": "order",
+    "paymentreminders": "reminder"
+}

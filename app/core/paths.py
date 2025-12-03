23bf03f9565e2e9c2d50bb45327c91bce62d57c3
Harsh Mukhiya <ha981muk@gmail.com>
2025-12-04 00:25:24 +0100

    path adjustments

--- app/core/paths.py
+++ app/core/paths.py
@@ -3,12 +3,11 @@ from pathlib import Path
 import sys
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
-APP_DIR = PROJECT_ROOT / "app"
-
 
 # Add project root for imports
 sys.path.append(str(PROJECT_ROOT))
 
+APP_DIR = PROJECT_ROOT / "app"
 
 DATA_DIR = APP_DIR / "data"
 
@@ -18,14 +17,4 @@ SYNTHETIC_DIR = DATA_DIR / "synthetic"
 # Output directory (Where the CSV will go)
 PROCESSED_DIR = DATA_DIR / "processed"
 
-# OUTPUT_FILENAME = "labeled_documents.csv"
 
-# Mapping Directory Names -> Target Labels
-# Key = Folder Name, Value = Label for CSV
-LABEL_MAP = {
-    "complaints": "complaint",
-    "contracts": "contract",
-    "invoices": "invoice",
-    "orders": "order",
-    "paymentreminders": "reminder"
-}
